66efdfb2ead42c5a6d045c6ba82613a3fd2a32a1
Nathan Ross <nross.se@gmail.com>
2014-07-15 23:57:53 -0400

    further refinement of build file

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var concat = require('gulp-concat');
 //var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var mixotroph = require('gulp-mixotroph');
+var rename = require('gulp-rename');
 //var lazypipe = require('lazypipe');
 var exec = require('child_process').exec;
 
@@ -16,34 +17,39 @@ function toShell(err, stdout, stderr) {
 }
 
 gulp.task('cleanTmp', function() {
-    return gulp.src("tmp/*.cpp", {read: false})
+    return gulp.src('tmp/*.cpp', {read: false})
         .pipe(clean());
 });
 gulp.task('cleanASM', ['cleanTmp'], function() {
-    return gulp.src("build/ASM/*", {read: false})
+    return gulp.src('build/asm/*', {read: false})
         .pipe(clean());
 });
 gulp.task('cleanFFI', ['cleanTmp'], function() {
-    return gulp.src("build/FFI/*", {read: false})
+    return gulp.src('build/ffi/*', {read: false})
         .pipe(clean());
 });
 
-gulp.task('cleanAll', ["cleanASM", "cleanFFI"]);
+gulp.task('cleanAll', ['cleanASM', 'cleanFFI']);
 
 gulp.task('prepareSource', function() {
-    gulp.src(["lib/libjson*","tmp/newlinebuffer.c","src/linprima.cpp"])
-    .pipe(concat("src.cpp"))
-    .pipe(gulp.dest("tmp"));
+    gulp.src(['lib/libjson*','tmp/newlinebuffer.c','src/linprima.cpp'])
+    .pipe(concat('src.cpp'))
+    .pipe(gulp.dest('tmp'));
 });
+
+function completeWrapper(mixMode, mixSnippetPath, dest) {
+   return gulp.src('src/linprima-wrap.js')
+    .pipe(mixotroph({mode:mixMode, snippetPath:mixSnippetPath}))
+    .pipe(rename('linprima.js'))
+    .pipe(gulp.dest(dest));
+}
 
 gulp.task('ffiWrapper', function() {
-   return gulp.src("src/linprima-wrap.js")
-    .pipe(mixotroph({mode:"FFI", snippetdir:"tmp/"}))
-    .pipe(gulp.dest("build/ffi/"));
+    return completeWrapper('FFI', 'src/snippets', 'build/ffi');
 });
 
-gulp.task('fficomp', ["cleanFFI", "fficomp", "ffiWrapper"]);
-gulp.task('ffiprod', ["cleanFFI", "fficomprod", "ffiWrapper"]);
+gulp.task('fficomp', ['cleanFFI', 'fficomp', 'ffiWrapper']);
+gulp.task('ffiprod', ['cleanFFI', 'fficomprod', 'ffiWrapper']);
 
 
 gulp.task('ffic', ['prepareSource'], function() {
@@ -54,18 +60,19 @@ gulp.task('ffip', ['prepareSource'], function() {
     exec("clang++ -O2 -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", toShell);
 });
 
+function asmCompDone(err, stdout, stderr) {
+    //asm requires code to be built before completing wrapper
+    //as code is substituted into wrapper.
+    toShell(err, stdout, stderr);
+    completeWrapper('ASM', 'tmp/', 'build/asm/');
+}
+
 //#compiled code is shimmed into js module,
 //#so this must be part of pipeline.
-gulp.task('asmc', ['prepareSource'], function() {
-    exec("emcc -std=c++11 -s EXPORTED_FUNCTIONS=\"['parseExtern', 'tokenizeExtern']\" tmp/src.cpp -o tmp/lin.asm.js", toShell);
-    return gulp.src("src/linprima.js")
-        .pipe(mixotroph({mode:"ASM",snippetdir:"tmp/"}))
-        .pipe(gulp.dest("build/asm/"));
+gulp.task('asmc', ['cleanASM', 'prepareSource'], function() {
+    exec("emcc -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.js", asmCompDone);
 });
 
 gulp.task('asmp', ['cleanASM', 'prepareSource'], function() {
-    exec("emcc -O3 -std=c++11 -s EXPORTED_FUNCTIONS=\"['parseExtern', 'tokenizeExtern']\" tmp/src.cpp -o tmp/lin.asm.js", toShell);
-    return gulp.src("src/linprima.js")
-        .pipe(mixotroph({mode:"ASM",snippetdir:"tmp/"}))
-        .pipe(gulp.dest("build/asm/"));
+    exec("emcc -O3 -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.js", asmCompDone);
 });
