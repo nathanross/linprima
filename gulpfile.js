f675215656dc6030b36e053dc68674d338eefb11
Nathan Ross <nross.se@gmail.com>
2014-07-18 07:58:32 -0400

    making more idiomatic gulp, also fixing asmc sync bug

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var rename = require('gulp-rename');
 //var lazypipe = require('lazypipe');
 var exec = require('child_process').exec;
 var replace = require('gulp-replace');
+var run = require('gulp-run');
+var runSequence = require('run-sequence');
 
 function toShell(err, stdout, stderr) {
     console.log(stdout);
@@ -18,7 +20,7 @@ function toShell(err, stdout, stderr) {
 }
 
 gulp.task('cleanTmp', function() {
-    return gulp.src('tmp/*.cpp', {read: false})
+    return gulp.src(['tmp/*.cpp', 'tmp/*.js'], {read: false})
         .pipe(clean());
 });
 gulp.task('cleanASM', ['cleanTmp'], function() {
@@ -45,51 +47,84 @@ function completeWrapper(mixMode, mixSnippetPath, dest) {
     .pipe(gulp.dest(dest));
 }
 
+//node neads this for readFileSync to work on gulp dests, which are flushed to disk asynchronously.
+
+gulp.task('sync', function(callback) {
+//    var cb = function(a,b,c) { toShell(a,b,c); callback(); };    
+    exec("sync",
+        makeExecCallback(callback));
+});
+
+function makeExecCallback(func) {
+    var r = function(a,b,c) {
+        toShell(a,b,c);
+        func();
+    }
+    return r;
+}
+
+
+gulp.task('fficcall', ['prepareSource'], function(callback) {
+    exec("clang++ -Wall -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", 
+        makeExecCallback(callback));
+});
+
+gulp.task('ffipcall', ['prepareSource'], function() {
+    exec("clang++ -O2 -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", 
+        makeExecCallback(callback));
+});
+
 gulp.task('ffiWrapper', function() {
     return completeWrapper('FFI', 'src/snippets', 'build/ffi');
 });
 
-gulp.task('fficomp', ['cleanFFI', 'fficomp', 'ffiWrapper']);
-gulp.task('ffiprod', ['cleanFFI', 'fficomprod', 'ffiWrapper']);
-
-
-gulp.task('ffic', ['prepareSource'], function() {
-    exec("clang++ -Wall -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", toShell);
+gulp.task('ffic', function() {
+    runSequence('cleanFFI', 'fficcall', 'ffiWrapper');
+});
+gulp.task('ffip', function() {
+    runSequence('cleanFFI', 'ffipcall', 'ffiWrapper');
 });
 
-gulp.task('ffip', ['prepareSource'], function() {
-    exec("clang++ -O2 -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", toShell);
-});
-
-function asmCompDone(err, stdout, stderr) {
     //asm requires code to be built before completing wrapper
     //as code is substituted into wrapper.
-    toShell(err, stdout, stderr);
-    completeWrapper('ASM', 'tmp/', 'build/asm/');
+gulp.task('asmccall', ['prepareSource'], function(callback) {
+    var cb = function(a,b,c) { toShell(a,b,c); callback(); };
+    exec("emcc -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.0.js", 
+        makeExecCallback(callback));
+});
+
+gulp.task('asmpcall', ['prepareSource'], function(callback) {
+//    var cb = function(a,b,c) { toShell(a,b,c); callback(); };    
+    exec("emcc -O3 -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.0.js",
+        makeExecCallback(callback));
+});
 
     //emscripten uses an export member on its module
-    //which, when using emscripten's choice of UMD pattern
+    //which, when using esprima's choice of UMD pattern
     //causes a name collision in NodeJS / commonJS module handling
     //leading the emscripten module itself to be returned 
     //rather than the wrapper.
-    gulp.src('tmp/linprima.asm.0.js')
+
+gulp.task('umdNodeAsmFix', function() {
+    var b = gulp.src('tmp/linprima.asm.0.js')
     .pipe(replace('export','emscport'))
     .pipe(rename('linprima.asm.js'))
     .pipe(gulp.dest('tmp'));
-    gulp.src('build/asm/linprima.js')
+});
+
+gulp.task('asmWrapper', function() {
+    completeWrapper('ASM', 'tmp/', 'build/asm/');
+});
+
+gulp.task('passToTest', function() {
+    return gulp.src('build/asm/linprima.js')
     .pipe(rename('esprima.js'))
     .pipe(gulp.dest('esprima/esprima/'))
-}
-
-//#compiled code is shimmed into js module,
-//#so this must be part of pipeline.
-gulp.task('asmcopy', function() {
-    asmCompDone(null, "", "");
-});
-gulp.task('asmc', ['cleanASM', 'prepareSource'], function() {
-    exec("emcc -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.0.js", asmCompDone);
 });
 
-gulp.task('asmp', ['cleanASM', 'prepareSource'], function() {
-    exec("emcc -O3 -std=c++11 -s EXPORTED_FUNCTIONS=\"['_parseExtern', '_tokenizeExtern']\" tmp/src.cpp -o tmp/linprima.asm.0.js", asmCompDone);
+gulp.task('asmc', function(callback) {
+    runSequence('cleanASM', 'asmccall', 'umdNodeAsmFix', 'sync', 'asmWrapper', 'passToTest', callback);
+});
+gulp.task('asmp', function(callback) {
+    runSequence('cleanASM', 'asmpcall', 'umdNodeAsmFix', 'sync', 'asmWrapper', 'passToTest', callback);
 });
