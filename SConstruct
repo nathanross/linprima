cde6a9e1ae4763eedc0fe49bd1009a05ead539e9
Nathan Ross <nross.se@gmail.com>
2014-09-06 22:39:51 -0400

    new build system. tested with clang, asm, profiny, and massif modes
    call through gulp. any options (non arguments only) will be passed transparently to scons for compilation.
    arguments such as 'asmjs' will obviously pass the appropriate implied option ('asmjs=1') without you providing
    it and add the corresponding wrapper after compilation.

---
+++ SConstruct
@@ -0,0 +1,185 @@
+#!/usr/bin/python
+import glob, os, sys, inspect, shutil
+from os import path
+#cmd_subfolder = path.realpath(path.abspath(path.join(path.split(inspect.getfile( inspect.currentframe() ))[0],"tools")))
+#if cmd_subfolder not in sys.path:
+#     sys.path.insert(0, cmd_subfolder)
+
+import throw52
+
+#general options
+log = int(ARGUMENTS.get('log', 0))
+
+C_ASM = 1;
+C_CLANG = 2;
+
+#-----------------------
+#detect compiler used
+c_asmjs = int(ARGUMENTS.get('asmjs', 0))
+c_clang = int(ARGUMENTS.get('clang', 0))
+c_gcc = int(ARGUMENTS.get('gcc', 0))
+
+total = c_asmjs + c_clang + c_gcc
+if total > 1:
+    print("too many compilers selected")
+    sys.exit()
+elif total == 0:
+    c_clang = 1
+
+
+#----------------------
+#asmjs compilation.
+if c_asmjs:
+   optim = int(ARGUMENTS.get('optim', 5))
+   gdb = 0
+   t52 = 1
+   profiny = 0
+   lowmem = int(ARGUMENTS.get('lowmem', 1))
+   limitjson = int(ARGUMENTS.get('limj', 1))
+#-----------------------
+#c++ binary compilation
+
+
+if c_clang or c_gcc:
+    optim = int(ARGUMENTS.get('optim', 3))
+
+    #debug with valgrind     
+    gdb = int(ARGUMENTS.get('gdb', 0))
+
+    #use asmjs stack-based exceptions instead of throws.    
+    t52 = int(ARGUMENTS.get('t52', 0))
+
+    profiny = int(ARGUMENTS.get('profiny', 0))
+    if profiny:
+        graph = int(ARGUMENTS.get('graph', 0)) 
+
+    #minimize memory usage at some performance
+    #cost (usually only useful for asmjs debug)
+    lowmem = int(ARGUMENTS.get('lowmem', 1))
+    limitjson = int(ARGUMENTS.get('limj', 1))
+
+#-----------------------------
+
+def clean(loc, preserveRoot=True):
+    if path.exists(loc):
+        shutil.rmtree(loc)
+    if preserveRoot:
+        os.mkdir(loc)
+
+#-----------------------------
+#compiler exec
+comp = ""
+if c_clang:
+    comp = "clang++"
+elif c_gcc:
+    comp = "g++"
+elif c_asmjs:
+    comp = "/usr/bin/emcc"
+
+#source preperation.
+root = "."
+srcroot = "."
+tmp_path = root + '/tmp'
+
+if t52 or profiny:
+    #do this first because could cause loss
+    #of work to call clean on a variable that
+    #may be, with only an accidental unindent,
+    #the repo root dir
+    for ldir in ['/src', '/include']:
+        l_destdir = tmp_path+ldir
+        clean(l_destdir, True)
+        l_srcdir = root+ldir
+        l_srcfiles = glob.glob(l_srcdir + '/*.cpp')
+        l_srcfiles.extend(glob.glob(l_srcdir + '/*.h'))
+        l_srcfiles.extend(glob.glob(l_srcdir + '/*.hpp'))
+        srcdestpairs = []
+        for l_srcfname in l_srcfiles:
+            l_destfname = l_destdir+'/'+path.basename(l_srcfname)
+            srcdestpairs.append({'in':l_srcfname, 'out':l_destfname})
+        if t52:       
+            params = throw52.getDefaultParams() 
+            throw52.Throw52Task(srcdestpairs, params)
+        elif profiny:
+            for srcdest in srcdestpairs:
+                if t52:
+                    srcdest['in'] = srcdest['out']
+                f_src = open(srcdest['in'], 'r')
+                data = f_src.read()
+                data = data.replace('DEBUGIN("', '\n PROFINY_SCOPE \n DEBUGIN("')
+                f_src.close()
+                f_out = open(srcdest['out'], 'w')
+                f_out.write(data)
+                f_out.flush()
+                f_out.close()
+    srcroot = tmp_path
+
+
+if c_clang or c_gcc:
+    if gdb:
+        l_out = tmp_path +'/test_gdb'  
+    elif profiny:
+        l_out = tmp_path +'/test_profiny'
+    else:
+        l_out = root+'/build/linprima.so'
+else:
+   l_out = tmp_path+'/linprim.asm.raw.js'
+
+linkflagstr = ' -Wall -std=c++11 -O'
+flagstr =' -Wall -std=c++11 -O'
+
+if optim <= 4:
+    flagstr += str(optim)
+    linkflagstr += str(optim)
+else:
+    flagstr += 'z '
+    linkflagstr += 'z '
+
+if profiny:
+   flagstr += ' -D PROFINY '
+   linkflagstr += ' -lboost_timer -lboost_chrono -lboost_system '
+   if graph:
+      flagstr += ' -D PROFINY_CALL_GRAPH_PROFILER '
+   else:
+      flagstr += ' -D PROFINY_FLAT_PROFILER '
+
+if c_gcc:
+    flagstr += ' -D LIBSTDC '
+else:
+    flagstr += ' -stdlib=libc++ '
+    linkflagstr += ' -stdlib=libc++ '
+
+if c_clang or c_gcc:
+    if profiny:
+        flagstr += ' -D HASMAIN '
+    elif gdb:
+        flagstr += ' -g -D HASMAIN '
+    else:
+        flagstr += ' -fPIC '
+        linkflagstr += ' -shared -fPIC '
+elif c_asmjs:
+    flagstr += " -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"['_parseASMJS', '_tokenizeASMJS']\" "
+    linkflagstr += " -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"['_parseASMJS', '_tokenizeASMJS']\" "
+
+if not t52:
+    flagstr += " -D THROWABLE "
+    
+if log:
+    flagstr += " -D DO_DEBUG "
+
+if limitjson:
+    flagstr += " -D LIMITJSON -D LOWMEM "
+elif lowmem:
+    flagstr += " -D LOWMEM "
+
+
+pathinfo = {'PATH' : os.environ['PATH']}
+#SConscript('SConscript', exports=['comp', 'root', 'srcroot', 'l_out', 'flagstr', 'linkflagstr', 'pathinfo'])
+
+env = Environment(
+    ENV = pathinfo,
+    CXX=comp,
+    CPPPATH=[root+'/lib', srcroot+'/include'],
+    CXXFLAGS=flagstr,
+    LINKFLAGS=linkflagstr)
+env.Program(l_out, Glob(srcroot+'/src/*.cpp'))
--- gulpfile.js
+++ gulpfile.js
@@ -7,79 +7,53 @@ var clean = require('gulp-clean');
 var mixotroph = require('gulp-mixotroph');
 var rename = require('gulp-rename');
 //var lazypipe = require('lazypipe');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var replace = require('gulp-replace');
 var run = require('gulp-run');
 var runSequence = require('run-sequence');
+var fs = require('fs');
 
-function toShell(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    if (err != null)
-        { console.log(err); }
-}
-
-function makeExecCallback(func) {
-    var r = function(a,b,c) {
-        toShell(a,b,c);
-        func();
-    }
-    return r;
-}
-
+//--- clean js wrappers ---------------------------------------
 gulp.task('cleanTmp', function() {
-    return gulp.src(['tmp/*.cpp', 'tmp/*.js'], {read: false})
+    return gulp.src('tmp/*', {read: false})
         .pipe(clean());
 });
-gulp.task('cleanASM', ['cleanTmp'], function() {
+
+//--- tooling ---------------------------------------------------
+//gulp.task('makeCompressors', function(callback) { //gperftools
+//    exec("./tools/compression/./init.sh", 
+//        makeExecCallback(callback));
+//});
+
+//--- compilation (call scons) --------------------------------
+gulp.task('compile', function(callback) {
+    args = []
+    var convertVal = function(val) {
+        if (val === true){
+            return 1;
+        }
+        return val;
+    }
+    for (var i in argv) {
+        if (i != '$0' && i != '_') {
+            args.push(i + '=' + convertVal(argv[i]));
+        }
+    }
+    var child = spawn('scons', args, {detached: false,
+                          stdio:[process.stdin, 
+                                 process.stdout, 
+                                 process.stderr]});
+    child.on('exit', callback);
+});
+
+//--- js wrapping -------------------------------------------------
+gulp.task('cleanASM', function() {
     return gulp.src('build/asm/*', {read: false})
         .pipe(clean());
 });
-gulp.task('cleanFFI', ['cleanTmp'], function() {
+gulp.task('cleanFFI', function() {
     return gulp.src('build/ffi/*', {read: false})
         .pipe(clean());
-});
-
-gulp.task('cleanAll', ['cleanASM', 'cleanFFI']);
-
-gulp.task('noThrow52', function() {
-    return;
-// gulp.src('src/linprima.cpp')
-  //  .pipe(gulp.dest('tmp'));
-});
-
-SRC_ORDER=['include/debug.hpp','include/stringutils.h', 'include/charutils.h', 'include/enums.h', 'include/podt.h', 'include/jsonutils.h', 'include/JsonDecompressor.h', 'include/Node.h', 'include/constants.h', 'include/NodesComments.h', 'include/LinprimaTask.h','include/WrappingNode.h', 'include/Tokenizer.h', 'include/parsepod.h', 'include/ParseFuncs.h', 'src/podt.cpp', 'src/Node.cpp', 'src/NodesComments.cpp', 'src/LinprimaTask.cpp', 'src/Tokenizer.cpp', 'src/ParseFuncs.cpp', 'src/JsonDecompressor.cpp', 'src/linprima.cpp']
-//'src/podt.h','src/jsonutils.h','src/Node.h','src/constants.h', 'src/NodesComments.h', 'src/LinprimaTask.h', 'src/Tokenizer.h', 'src/LinprimaTask.cpp', 'src/Tokenizer.cpp', 'src/NodesComments.cpp', 'src/linprima.cpp']
-
-gulp.task('joinSrc', function() {
-    return gulp.src(SRC_ORDER)
-    .pipe(concat('linprima.cpp'))
-    .pipe(gulp.dest('tmp'));
-});
-
-gulp.task('joinSrcProfiny', function() {
-    return gulp.src(SRC_ORDER)
-    .pipe(concat('linprima.cpp'))
-    .pipe(replace('DEBUGIN("', '\n PROFINY_SCOPE \n DEBUGIN("'))
-    .pipe(gulp.dest('tmp'));
-});
-
-gulp.task('throw52', function(callback) { //gperftools
-    exec("python src/throw52.py -v 0 tmp/linprima.cpp tmp/linprima2.cpp && mv tmp/linprima2.cpp tmp/linprima.cpp", 
-        makeExecCallback(callback));
-});
-
-gulp.task('makeCompressors', function(callback) { //gperftools
-    exec("./tools/compression/./init.sh", 
-        makeExecCallback(callback));
-});
-
-gulp.task('addLibs', function() {
-    return gulp.src(['lib/rapidjson*',
-                     'tools/compression/cmpR_out/jsoncompress.cpp',
-                     'tmp/linprima.cpp'])
-    .pipe(concat('src.cpp'))
-    .pipe(gulp.dest('tmp'));
 });
 
 function completeWrapper(mixMode, mixSnippetPath, dest) {
@@ -89,97 +63,13 @@ function completeWrapper(mixMode, mixSnippetPath, dest) {
     .pipe(gulp.dest(dest));
 }
 
-//node neads this for readFileSync to work on gulp dests, which are flushed to disk asynchronously.
-
-gulp.task('sync', function(callback) {
-    exec("sync",
-        makeExecCallback(callback));
-});
-
-//profiny
-gulp.task('ffitcall', function(callback) {
-     exec("clang++ -Wall -O3 -D THROWABLE -D HASMAIN -D USE_PROFINY -D" + 
-          ((argv.graph !== undefined)? 
-           "PROFINY_CALL_GRAPH_PROFILER":
-           "PROFINY_FLAT_PROFILER") + 
-          " -std=c++11 -stdlib=libc++ " +
-          " -lboost_timer -lboost_chrono -lboost_system" + 
-          " -o \"profiny_test\" \"tmp/src.cpp\"", 
-        makeExecCallback(callback));
-});
-
-//valgrind/gdb
-gulp.task('ffidcall', function(callback) { //gdb, valgrind
-    exec("clang++ -g -O3 -Wall -D HASMAIN -D THROWABLE " +
-         ((argv.lowmem)? " -D LOWMEM ": "") + 
-         ((argv.limj)? " -D LOWMEM -D LIMITJSON ": "") + 
-         ((argv.dbg)? " -D DO_DEBUG ": "") + 
-         " -std=c++11 -stdlib=libc++ tmp/src.cpp -o test.out", 
-        makeExecCallback(callback));
-});
-
-//general testing. mirrors production compilation, 
-//except no optimization, opt. print out call graph
-//opt. use throw52 for easier/faster debugging than emcc
-// of any throw52 interactions causing problems.
-gulp.task('fficcall', function(callback) {
-     exec("clang++ -Wall -D LOWMEM -D LIMITJSON " + 
-          ((argv.t52 !== undefined)? "":" -D THROWABLE ") + 
-          ((argv.dbg !== undefined)? " -D DO_DEBUG ":"") + 
-          " -std=c++11 -stdlib=libc++ -shared -fPIC -o build/ffi/linprima.x64.so tmp/src.cpp", 
-        makeExecCallback(callback));
-});
-
-gulp.task('ffipcall', function(callback) {
-    exec("clang++ -O3 -D THROWABLE -std=c++11 -stdlib=libc++ -shared -fPIC tmp/src.cpp -o build/ffi/linprima.x64.so", 
-        makeExecCallback(callback));
-});
-
-
-
 gulp.task('ffiWrapper', function() {
     return completeWrapper('FFI', 'src/snippets', 'build/ffi');
 });
-gulp.task('ffi', function() { //gdb and valgrind debugging
-    runSequence('cleanFFI', 
-               (argv.profiny !== undefined)? 
-                'joinSrcProfiny' : 'joinSrc', 
-               (argv.t52 !== undefined)? 'throw52':'noThrow52',
-               'addLibs',
-               (argv.p !== undefined)? 'ffipcall':
-               ((argv.g !== undefined)? 'ffigcall': //call like this: gulp ffi --g
-               ((argv.d !== undefined)? 'ffidcall': 
-               ((argv.profiny !== undefined)? 'ffitcall' : 'fficcall'))),
-                'ffiWrapper');
-});
-
-    //asm requires code to be built before completing wrapper
-    //as code is substituted into wrapper.
-gulp.task('asmccall', function(callback) {
-    exec("emcc -std=c++11 -O3 -D LOWMEM -D LIMITJSON   -s ALIASING_FUNCTION_POINTERS=0 " +
-         ((argv.dbg !== undefined)? " -D DO_DEBUG " : "") +
-         " -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"['_parseASMJS', '_tokenizeASMJS']\" tmp/src.cpp -o tmp/linprima.asm.0.js", 
-        makeExecCallback(callback));
-});
-
-gulp.task('asmpcall', function(callback) {
-    exec("/opt/emscripten/emcc -Oz -std=c++11 -D LOWMEM -D LIMITJSON -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"['_parseASMJS', '_tokenizeASMJS']\" tmp/src.cpp -o tmp/linprima.asm.0.js",
-        makeExecCallback(callback));
-});
-gulp.task('asmtcall', function(callback) {
-    exec("emcc -profiling -std=c++11 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"['_parseASMJS', '_tokenizeASMJS']\" tmp/src.cpp -o tmp/linprima.asm.0.js",
-        makeExecCallback(callback));
-});
-
-    //emscripten uses an export member on its module
-    //which, when using esprima's choice of UMD pattern
-    //causes a name collision in NodeJS / commonJS module handling
-    //leading the emscripten module itself to be returned 
-    //rather than the wrapper.
 
 gulp.task('umdFixes', function() {
-    var b = gulp.src('tmp/linprima.asm.0.js')
-    .pipe(replace('export','emscport')) //nodejs module importing
+    var b = gulp.src('tmp/linprim.asm.raw.js')
+    .pipe(replace('export','emccport')) //nodejs module importing
     .pipe(rename('linprima.asm.js'))
     .pipe(gulp.dest('tmp'));
 });
@@ -188,23 +78,119 @@ gulp.task('asmWrapper', function() {
     completeWrapper('ASM', 'tmp/', 'build/asm/');
 });
 
-gulp.task('passToTest', function() {
-    return gulp.src('build/asm/linprima.js')
-    .pipe(rename('esprima.js'))
-    .pipe(gulp.dest('esprima/esprima/'))
+//node neads this for readFileSync to work on gulp dests, which are flushed to disk asynchronously.
+gulp.task('sync', function(callback) {
+    var child = spawn('sync', [], {});
+    child.on('exit', callback);
 });
 
-gulp.task('asm', function(callback) {
-    runSequence('cleanASM',
-                'joinSrc',
-                'throw52',                
-                'addLibs',
-                (argv.p !== undefined) ? 'asmpcall' : 
-                (argv.prof !== undefined) ? 'asmtcall' :'asmccall', 
-                'umdFixes', 
-                'sync',
-                'asmWrapper',  
-                'passToTest', 
-                callback);
+//--- compilation + js wrapping ------------------------------------------
+gulp.task('asmjs', function() {
+    argv.asmjs = 1;
+    runSequence('compile', 'umdFixes', 'sync', 'asmWrapper');
+});
+gulp.task('clang', function() {
+    argv.clang = 1;
+    runSequence('compile', 'ffiWrapper');
+});
+gulp.task('gcc', function() {
+    argv.gcc = 1;
+    runSequence('compile', 'ffiWrapper');
 });
 
+//--- speed profiling shortcuts ----------------------------------------
+gulp.task('runproftest', function(callback) {
+    var child = spawn('./tmp/./test_profiny', [], {})
+    child.on('exit', function() { 
+        fs.rename('profiny.out', 'tmp/profiny.out', callback);
+    });
+});
+
+gulp.task('processProfinyOut', function(callback) {
+    var execPath = './tools/profiny/./'
+    if (argv.graph) {
+        execPath += 'graphprocess.py'
+    } else {
+        execPath += 'flatprocess.sh'
+    }
+    var child = spawn(execPath, [], {})
+    child.on('exit', callback);
+});
+
+gulp.task('prof', function() {
+    argv.clang = 1;
+    argv.profiny = 1;
+    runSequence('compile', 'runproftest', 'processProfinyOut');
+});
+
+//--- memory profiling shortcuts ---------------------------------------
+var memstep=""
+var memOutfolder=""
+var tmpPath = 'tmp/'
+
+var massif = function(callback, heap) {
+    try {
+        fs.mkdirSync(tmpPath+'mem/')
+    } catch(e) {}
+    try {
+        fs.mkdirSync(tmpPath+'mem/'+memOutfolder)
+    } catch(e) {} 
+    var l_out = tmpPath+'mem/'+memOutfolder+memstep + 
+        ((heap)?'heap':'both')
+    var args = ['--tool=massif', 
+                '--max-snapshots=150',
+                '--stacks='+((heap)?'no':'yes'),
+                '--massif-out-file='+l_out,
+                tmpPath+'test_gdb']
+    var valgrind = spawn('valgrind', args, {'stdio':[null, 
+                                                     process.stdio, 
+                                                     process.stderr]});
+    valgrind.on('exit', function() {
+        var f_out = fs.openSync(l_out+'_msprint', 'a');
+        var ms_print = spawn('ms_print', 
+                             [l_out], 
+                             {detached: true, stdio:[null, f_out, null]});
+        ms_print.unref();
+        if (heap) { massif(callback, false); }
+        else { callback(); }
+
+    });
+}
+gulp.task('massif', function(callback) { massif(callback, true); });
+
+gulp.task('incrMemStep', function(callback) {
+    if (memstep == "reg") {
+        argv.lowmem = 1;
+        memstep = "lowmem"
+    } else if (memstep == "lowmem") {
+        argv.limj = 1;
+        memstep = "limj"
+    }
+    callback();
+});
+
+gulp.task('mem', function() {
+    argv.clang = 1;
+    argv.gdb = 1;
+    //get it so folders are ordered consecutively.
+    memOutfolder = String((new Date()).getTime()).substr(6,4) + "/"
+
+    argv.lowmem = 0;
+    argv.limj = 0;
+    memstep = "reg"
+
+    runSequence('compile', 'massif', 'incrMemStep',
+               'compile', 'massif', 'incrMemStep',
+               'compile', 'massif');
+});
+
+//--- build prod ------------------------------------------
+gulp.task('prod', function() {
+    argv = {'asmjs':1}
+    runSequence('compile', 'umdFixes', 'sync', 'asmWrapper');
+    argv = {'clang':1}
+    runSequence('compile', 'ffiWrapper');
+});
+
+//-- clean everything ------------------------------------
+gulp.task('cleanAll', ['cleanASM', 'cleanFFI', 'cleanTmp']);
